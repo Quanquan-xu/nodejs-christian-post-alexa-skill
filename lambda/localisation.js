85f06ab756c189965635e1e3c46094a3ee5badff
Joseph Xu <13371603724a@gmail.com>
2021-04-19 14:38:11 -0400

    enable christianpodcast branch

--- lambda/localisation.js
+++ lambda/localisation.js
@@ -1,7 +1,8 @@
 module.exports = {
     en: {
         translation: {
-            WELCOME_MSG: `Welcome to the Edifi Podcasts. Edifi Podcasts is a Christian-centric powerful podcast player, you can say, begin the podcast, to start, or say help, to check more functions! `,
+            WELCOME_MSG_INIT: `Welcome to the Christian Podcast. With the Christian Podcast, you can easily listen to the latest episodes featured on Edifi along with popular channels, you can say, begin the podcast, to start, or say help, to check more functions! `,
+            WELCOME_MSG: `Welcome to the Christian Podcast, you can say, begin the podcast, to start, or say help, to check more functions! `,
             WELCOME_MSG_REPROMPT: 'You can say, begin the podcast, to start.',
             WELCOME_BACK_MSG: `You were listening to {{description}} . Would you like to resume?`,
             WELCOME_BACK_MSG_REPROMPT: 'You can say yes to resume or no to next step.',
@@ -10,7 +11,7 @@ module.exports = {
             REFLECTOR_MSG: 'You just triggered {{intent}}',
             ERROR_MSG: 'Sorry, there was an error. Please try again.',
             DEVELOPING_MSG: 'Sorry, This feature is not currently supported!',
-            BUILT_IN_HELP_WEL_VERSE:'Welcome to the Edifi Podcasts! Edifi is a collection of transformational and faith-inspiring podcasts. ',
+            BUILT_IN_HELP_WEL_VERSE:'Welcome to the Christian Podcast! Christian Podcast can help you easily listen to the latest episodes along with popular channels on Edifi. ',
             HELP_IN_LISENING_MSG: `You are listening to  {{description}} . You can say, Alexa, see playlist, to look through the playlist. You can also say, Alexa, see channel list, to check recommended channels! Or say, Alexa, search channels or episodes, to search channels or episodes. `,
             HELP_IN_QUESTION_MSG: `You can see channels or featured episodes list by saying, Alexa, see channel list , or Alexa, see featured episodes list. You can use numbers, instead of names by saying, Alexa, episode {{number1}} or channel {{number2}}, to start the podcast. `,
             HELP_IN_SEARCH_MSG: ['You can also search channels or episodes by saying, Alexa, search channels, or Alexa, search episodes. ', 'Or say, Alexa, search, to search channels or episodes. '],
@@ -20,7 +21,7 @@ module.exports = {
             START_PLAYING_MSG: `This is  {{description}} . `,
             START_PLAYING_RESUME_MSG: `Last time, You have heard {{description}} around {{time}}. Would you like to resume? `,
             START_PLAYING_RESUME_MSG_REPROMPT: 'You can say yes to resume or no to play from the beigining.',
-            START_PLAYING_HELP_MSG: `You can say, Next or Previous to navigate through your playlist. You can also say Alexa, ask Edifi Podcasts see my playlist, to look through the playlist! Or say, Alexa, ask Edifi Podcasts play channel + number, to change channel!`,
+            START_PLAYING_HELP_MSG: `You can say, Next or Previous to navigate through your playlist. You can also say Alexa, ask Christian Podcast see my playlist, to look through the playlist! Or say, Alexa, ask Christian Podcast play channel + number, to change channel!`,
             LIST_PROMPT_NOTIFICATION_MSG: `You can use number, instead of name by saying, for example, alexa, {{name}} {{number}}, to start podcasts! `,
             LIST_ONLY_TOP_FIVE_NOTIFICATION_MSG: ` Here are top 5 {{name}} : `,
             LIST_PROMOTION_EPISODES_NOTIFICATION_MSG_FOR_CHANNEL: `{{length}} latest featured episodes are available, you can say alexa, play featured episodes, to change your playlist! `,
